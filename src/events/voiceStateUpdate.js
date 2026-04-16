688811eed10ff0b4c7827717fb38afc1aa4bdba2
Ruben Galeano <rubengaleano2003@gmail.com>
2026-04-16 14:31:17 +0200

    small caps font added

--- src/events/voiceStateUpdate.js
+++ src/events/voiceStateUpdate.js
@@ -2,6 +2,23 @@ const tempChannels = new Set();
 
 const CREATE_CHANNEL_ID = "1494306247929757806";
 
+// 🧠 función small caps
+function toSmallCaps(text) {
+    const map = {
+        a: "ᴀ", b: "ʙ", c: "ᴄ", d: "ᴅ", e: "ᴇ",
+        f: "ꜰ", g: "ɢ", h: "ʜ", i: "ɪ", j: "ᴊ",
+        k: "ᴋ", l: "ʟ", m: "ᴍ", n: "ɴ", o: "ᴏ",
+        p: "ᴘ", q: "q", r: "ʀ", s: "ꜱ", t: "ᴛ",
+        u: "ᴜ", v: "ᴠ", w: "ᴡ", x: "x", y: "ʏ", z: "ᴢ"
+    };
+
+    return text
+        .toLowerCase()
+        .split("")
+        .map(c => map[c] || c)
+        .join("");
+}
+
 module.exports = {
     name: "voiceStateUpdate",
 
@@ -14,8 +31,10 @@ module.exports = {
         ) {
             const guild = newState.guild;
 
+            const usernameStyled = toSmallCaps(newState.member.displayName);
+
             guild.channels.create({
-                name: `Sala de ${newState.member.user.username}`,
+                name: `🔊│ꜱᴀʟᴀ ᴅᴇ ${usernameStyled}`,
                 type: require('discord.js').ChannelType.GuildVoice,
                 parent: newState.channel.parent
             }).then(channel => {
